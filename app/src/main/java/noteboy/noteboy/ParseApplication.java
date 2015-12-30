32e37d5df39f657e60c332074eabfbc3e7dd41aa
Chirag Shenoy <chiragshenoy@gmail.com>
2015-12-30 13:36:33 +0530

    ParseApplication added

---
+++ app/src/main/java/noteboy/noteboy/ParseApplication.java
@@ -0,0 +1,31 @@
+package noteboy.noteboy;
+
+import android.app.Application;
+import android.support.v7.widget.ThemedSpinnerAdapter;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.interceptors.ParseStethoInterceptor;
+
+/**
+ * Created by Chirag Shenoy on 30-Dec-15.
+ */
+public class ParseApplication extends Application {
+
+    public boolean isDataLoggerServiceRunning = false;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+        Parse.addParseNetworkInterceptor(new ParseStethoInterceptor());
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "a8cAez0DOO17ghQEYQcB6Ni7nGTD0c6RkSwTWbj3", "SVLzidFiHWpRZXCBkwmQj2FxWum78N4884IKTSGX");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+    }
+}
